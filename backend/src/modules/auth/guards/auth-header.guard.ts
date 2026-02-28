b631360bdbfaa72c92da7f21afd47607b4a3e248
= <fayamir54@gmail.com>
2026-02-28 10:38:54 +0330

    refactor auth-guard

--- backend/src/modules/auth/guards/auth-header.guard.ts
+++ backend/src/modules/auth/guards/auth-header.guard.ts
@@ -4,14 +4,27 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { SKIP_AUTH } from '@shared/decorators/skip-auth.decorator';
 import { Request } from 'express';
 import { JwtAppService } from '../services/jwt.service';
 import { extractTokenFromHeader } from '@shared/utils/extract-token';
 
 @Injectable()
 export class AuthWithHeader implements CanActivate {
-  constructor(private readonly JwtService: JwtAppService) {}
+  constructor(
+    private readonly JwtService: JwtAppService,
+    private readonly reflector: Reflector,
+  ) {}
+
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const skipAuth = this.reflector.getAllAndOverride(SKIP_AUTH, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (skipAuth) return true;
+
     const request = context.switchToHttp().getRequest<Request>();
 
     try {
