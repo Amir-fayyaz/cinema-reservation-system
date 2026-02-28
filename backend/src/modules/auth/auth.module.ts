ca444de48dae533a6e92602ac6a345d0720ee565
= <fayamir54@gmail.com>
2026-02-28 22:19:56 +0330

    add AuthEventListener

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Global, Module } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthEventListener } from '@shared/event-listeners/auth/auth.eventListener';
 import { CacheService } from '@shared/services/cache.service';
 import { AuthController } from './controllers/auth.controller';
 import { Permission } from './entities/permission.entity';
@@ -13,7 +14,14 @@ import { OtpService } from './services/otp.service';
 @Module({
   imports: [TypeOrmModule.forFeature([User, Permission])],
   controllers: [AuthController],
-  providers: [AuthService, JwtService, JwtAppService, OtpService, CacheService],
+  providers: [
+    AuthService,
+    JwtService,
+    JwtAppService,
+    AuthEventListener,
+    OtpService,
+    CacheService,
+  ],
   exports: [JwtService, JwtAppService],
 })
 export class AuthModule {}
