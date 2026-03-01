3eff8b43146dc69402f8f413d54659dcf6c8787b
= <fayamir54@gmail.com>
2026-03-02 01:42:26 +0330

    fix authorization-decorator inputs

--- backend/src/common/decorators/authorization.decorator.ts
+++ backend/src/common/decorators/authorization.decorator.ts
@@ -2,10 +2,10 @@ import { SetMetadata } from '@nestjs/common';
 import { PERMISSIONS_KEY, ROLES_KEY } from '@shared/constants/authorizaton';
 import { ApplicationRoles } from '@shared/enums/role-app.enum';
 
-export const AuthorizeByPermission = (...permissions: string[]) => {
+export const AuthorizeByPermission = (permissions: string[]) => {
   return SetMetadata(PERMISSIONS_KEY, permissions);
 };
 
-export const AuthorizeByRole = (...roles: ApplicationRoles[]) => {
+export const AuthorizeByRole = (roles: ApplicationRoles[]) => {
   return SetMetadata(ROLES_KEY, roles);
 };
