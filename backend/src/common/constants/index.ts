489ab1911994b4e583dca08fca2100bc940de96e
= <fayamir54@gmail.com>
2026-03-02 15:48:14 +0330

    add index.ts forre-export

---
+++ backend/src/common/constants/index.ts
@@ -0,0 +1,6 @@
+export * from './authorizaton';
+export * from './events/auth.events';
+export * from './jwt';
+export * from './permissions/permission';
+export * from './permissions/permission-category';
+export * from './tokens';
