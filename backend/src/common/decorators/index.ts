f9f570da33a49294a08fe754b52b43a78d86e6fc
= <fayamir54@gmail.com>
2026-03-02 23:55:32 +0330

    add skip-cache decorator

--- backend/src/common/decorators/index.ts
+++ backend/src/common/decorators/index.ts
@@ -1,5 +1,6 @@
 export * from './authorization.decorator';
 export * from './cookie.decorator';
+export * from './no-cache.decorator';
 export * from './pagination-option.decorator';
 export * from './skip-auth.decorator';
 export * from './url.decorator';
