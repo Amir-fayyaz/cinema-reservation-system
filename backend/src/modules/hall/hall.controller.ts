30b6e7b79038ac9028da2a92ba852fcc0144b731
= <fayamir54@gmail.com>
2026-03-19 00:56:47 +0330

    add hall end-points

--- backend/src/modules/hall/hall.controller.ts
+++ backend/src/modules/hall/hall.controller.ts
@@ -1,7 +1,66 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CREATE_HALL, DELETE_HALL, UPDATE_HALL } from '@shared/constants';
+import {
+  AuthorizeByPermission,
+  PaginationOptions,
+  SkipAuth,
+  Url,
+} from '@shared/decorators';
+import { Paginate, PaginateQuery } from 'nestjs-paginate';
+import { CreateHallDto } from './dto/create-hall.dto';
+import { UpdateHallDto } from './dto/update-hall.dto';
 import { HallService } from './hall.service';
 
-@Controller('hall')
+@Controller('halls')
 export class HallController {
   constructor(private readonly service: HallService) {}
+
+  @Post()
+  @AuthorizeByPermission([CREATE_HALL])
+  async create(@Body() dto: CreateHallDto) {
+    return await this.service.create(dto);
+  }
+
+  @Get()
+  @SkipAuth()
+  @PaginationOptions({
+    sortOptions: [{ example: 'createdAt:DESC' }],
+    filterOptions: [
+      { field: 'name', example: '$eq:hall-name-1' },
+      { field: 'rows', example: '$lte:2', type: Number },
+    ],
+  })
+  async findAll(@Paginate() query: PaginateQuery, @Url() url: string) {
+    return await this.service.findAll(query, url);
+  }
+
+  @Get(':id')
+  @SkipAuth()
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return await this.service.findOne(id);
+  }
+
+  @Put(':id')
+  @AuthorizeByPermission([UPDATE_HALL])
+  async update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() dto: UpdateHallDto,
+  ) {
+    return await this.service.update(id, dto);
+  }
+
+  @Delete(':id')
+  @AuthorizeByPermission([DELETE_HALL])
+  async remove(@Param('id', ParseUUIDPipe) id: string) {
+    return await this.service.remove(id);
+  }
 }
--- backend/src/modules/hall/hall.service.ts
+++ backend/src/modules/hall/hall.service.ts
@@ -1,6 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import {
+  FilterOperator,
+  paginate,
+  Paginated,
+  PaginateQuery,
+} from 'nestjs-paginate';
 import { Repository } from 'typeorm';
+import { CreateHallDto } from './dto/create-hall.dto';
+import { UpdateHallDto } from './dto/update-hall.dto';
 import { Hall } from './entities/hall.entity';
 
 @Injectable()
@@ -9,5 +17,47 @@ export class HallService {
     @InjectRepository(Hall) private readonly repository: Repository<Hall>,
   ) {}
 
-  async create() {}
+  async create(dto: CreateHallDto): Promise<Hall> {
+    const hall = this.repository.create(dto);
+    return await this.repository.save(hall);
+  }
+
+  async findAll(query: PaginateQuery, url: string): Promise<Paginated<Hall>> {
+    return paginate(query, this.repository, {
+      sortableColumns: ['createdAt'],
+      defaultSortBy: [['createdAt', 'DESC']],
+      filterableColumns: {
+        name: [FilterOperator.EQ],
+        rows: [FilterOperator.LTE, FilterOperator.GTE, FilterOperator.EQ],
+      },
+      origin: url,
+    });
+  }
+
+  async findOne(id: string): Promise<Hall> {
+    const hall = await this.repository.findOne({ where: { id } });
+
+    if (!hall) throw new NotFoundException();
+
+    return hall;
+  }
+
+  async update(id: string, dto: UpdateHallDto) {
+    const hall = await this.findOne(id);
+
+    Object.assign(hall, dto);
+
+    return await this.repository.save(hall);
+  }
+
+  async remove(id: string) {
+    await this.exists(id);
+    return await this.repository.softDelete({ id });
+  }
+
+  private async exists(id: string) {
+    const exists = await this.repository.exists({ where: { id } });
+
+    if (!exists) throw new NotFoundException();
+  }
 }
