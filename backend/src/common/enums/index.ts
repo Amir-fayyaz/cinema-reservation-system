afadab3e554ccdcaad4f6342f4005e1eae8bed0c
= <fayamir54@gmail.com>
2026-03-22 02:22:31 +0330

    add screening-status enum

--- backend/src/common/enums/index.ts
+++ backend/src/common/enums/index.ts
@@ -3,3 +3,4 @@ export * from './cinema-status.enum';
 export * from './gender.enum';
 export * from './permission.enum';
 export * from './role-app.enum';
+export * from './screening-status.enum';
---
+++ backend/src/common/enums/screening-status.enum.ts
@@ -0,0 +1,4 @@
+export enum ScreeningStatus {
+  ACTIVE = 'active',
+  INACTIVE = 'inactive',
+}
