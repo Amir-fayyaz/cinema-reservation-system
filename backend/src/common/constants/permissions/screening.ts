22d184c50609fa1ee12528ade9d42c1081062554
= <fayamir54@gmail.com>
2026-03-22 14:46:35 +0330

    add screening-permissions

---
+++ backend/src/common/constants/permissions/screening.ts
@@ -0,0 +1,4 @@
+export const CREATE_SCREENING = 'create-screening';
+export const READ_SCREENING = 'read-screening';
+export const UPDATE_SCREENING = 'update-screening';
+export const DELETE_SCREENING = 'delete-screening';
