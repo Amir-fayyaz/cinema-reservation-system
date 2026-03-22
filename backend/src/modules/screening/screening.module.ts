7883c34e17970311223e9d9cb061d3eba01cabc0
= <fayamir54@gmail.com>
2026-03-23 01:49:34 +0330

    add repo dependency to module-providers

--- backend/src/modules/screening/screening.module.ts
+++ backend/src/modules/screening/screening.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Screening } from './entities/screening.entity';
 import { ScreeningController } from './screening.controller';
+import { ScreeningRepository } from './screening.repository';
 import { ScreeningService } from './screening.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Screening])],
   controllers: [ScreeningController],
-  providers: [ScreeningService],
+  providers: [ScreeningService, ScreeningRepository],
   exports: [],
 })
 export class ScreeningModule {}
